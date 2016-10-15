d079d90b3672678a901808c0509e24ab3a4156c2
comeonmissli <1312467852@qq.com>
2016-10-15 20:01:23 +0800

    ok

---
+++ gen/com/example/listviewtest10/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.listviewtest10;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/listviewtest10/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.listviewtest10;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+
+public class MainActivity extends Activity {
+	private String[] books=new String[]{"Android平台开发之旅","Android开发案例驱动教程",
+			  "Android揭秘","疯狂Android讲义","Android从零开始"};
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		ArrayAdapter<String> adapter=new ArrayAdapter<String> (this,
+				  android.R.layout.simple_list_item_1,books);
+		setListAdapter(adapter);
+	}
+
+	private void setListAdapter(ArrayAdapter<String> adapter) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
